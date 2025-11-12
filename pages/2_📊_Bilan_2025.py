5cf5185aa62f88117edfdd1644f4104058c447d0
a-videgain <a.videgain.capb@gmail.com>
2025-11-12 18:31:32 +0100

    Update 2_📊_Bilan_2025.py

--- pages/2_📊_Bilan_2025.py
+++ pages/2_📊_Bilan_2025.py
@@ -35,11 +35,11 @@ if 'km_2025_territoire' not in st.session_state:
 
 # Calcul du bilan 2025
 bilan_2025 = calculer_bilan_territoire(
-    tuple(st.session_state.km_2025_territoire.items()),
-    tuple({**st.session_state.emissions, 'emission_thermique': st.session_state.parc_2025['emission_thermique']}.items()),
-    tuple(st.session_state.parc_2025.items()),
-    tuple(st.session_state.parc_velo_2025.items()),
-    tuple(st.session_state.parc_bus_2025.items()),
+    st.session_state.km_2025_territoire,
+    {**st.session_state.emissions, 'emission_thermique': st.session_state.parc_2025['emission_thermique']},
+    st.session_state.parc_2025,
+    st.session_state.parc_velo_2025,
+    st.session_state.parc_bus_2025,
     reduction_poids=0
 )
 parts_2025 = calculer_parts_modales(st.session_state.km_2025_territoire)
